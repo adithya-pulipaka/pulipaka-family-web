ee9946b896d7f6209b894d423948750f519bf66e
Adithya Kashyap Pulipaka <adithya2205@gmail.com>
2025-11-10 22:53:51 -0600

    basic event integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,41 +11,75 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import Calendar26 from "@/components/calendar-26"
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Calendar26 from "@/components/calendar-26";
+import { useState } from "react";
+import axios from "axios";
 
+interface EventInfo {
+  eventName: string | null;
+  description: string | null;
+  eventStartTime: string | null;
+  eventEndTime: string | null;
+  location: string | null;
+}
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+  const [eventInfo, setEventInfo] = useState<EventInfo>({
+    eventName: null,
+    description: null,
+    eventStartTime: null,
+    eventEndTime: null,
+    location: null,
+  });
+
+  const updateEventDuration = (startTime: string, eventEndTime: string) => {
+    setEventInfo({
+      ...eventInfo,
+      eventStartTime: startTime,
+      eventEndTime: eventEndTime,
+    });
+  };
+
+  const onSubmit = async () => {
+    console.log(eventInfo);
+    eventInfo.description = "desc-1";
+    eventInfo.eventName = "hello there";
+    eventInfo.location = "hyderabad";
+    const response = await axios.post(
+      "http://localhost:8000/events",
+      eventInfo
+    );
+    console.log(response);
+    setOpen(false);
+  };
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+      <main className="flex min-h-screen w-full flex-col items-center py-16 px-8 bg-white dark:bg-black sm:items-start">
+        <div className="">
+          <Button
+            onClick={() => setOpen(true)}
+            className="hover:cursor-pointer"
+          >
+            Create Event
+          </Button>
+        </div>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          
-          <Button>Button</Button>
-
-          <Dialog>
+          <Dialog open={open} onOpenChange={setOpen}>
             <form>
-              <DialogTrigger asChild>
-                <Button variant="outline">Create Event</Button>
-              </DialogTrigger>
+              {/* <DialogTrigger asChild>
+                <Button>Create Event</Button>
+              </DialogTrigger> */}
               <DialogContent className="sm:max-w-[425px]">
                 <DialogHeader>
                   <DialogTitle>Event Details</DialogTitle>
                   <DialogDescription>
-                    Create a new Event using this form below. You can always update the event info later.
+                    Create a new Event using this form below. You can always
+                    update the event info later.
                   </DialogDescription>
                 </DialogHeader>
                 <div className="grid gap-4">
@@ -66,7 +102,9 @@ export default function Home() {
                     />
                   </div>
                   <div className="grid gap-3">
-                    <Calendar26></Calendar26>
+                    <Calendar26
+                      onEventDurationChange={updateEventDuration}
+                    ></Calendar26>
                   </div>
                   <div className="grid gap-3">
                     <Label htmlFor="eventLocation">Event Location</Label>
@@ -82,53 +120,13 @@ export default function Home() {
                   <DialogClose asChild>
                     <Button variant="outline">Cancel</Button>
                   </DialogClose>
-                  <Button type="submit">Save changes</Button>
+                  <Button type="submit" onClick={onSubmit}>
+                    Save changes
+                  </Button>
                 </DialogFooter>
               </DialogContent>
             </form>
           </Dialog>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
         </div>
       </main>
     </div>
